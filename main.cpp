3a818eea121d886f8400305c3e72dbe410e5bd89
zloydanny <dan.abdrakhimov@yandex.ru>
2018-04-05 23:06:47 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,30 @@
 //
 // Created by ruby on 22.03.18.
 //
-
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include <utility>
 #include "GameObject.cpp"
-#define GameObject StupidObject
-#include "StupidObject.cpp"
-#include "resourses.cpp"
-#include "Physics.cpp"
-#include "Drawing.cpp"
+
+//#include "resourses.cpp"
+//#include "Physics.cpp"
+
+
+
 
 int main(){
+
+    GameObject object;
+
+    object.addComponent<RigidBody>();
+
+
+    object.getComponent<RigidBody>().a = 15;
+    int b = object.getComponent<RigidBody>().a;
+    std::cout << b << std::endl;
+    std::cout << typeid(object.getComponent<RigidBody>().a).name() << std::endl;
+
+
 
     return 0;
 }
