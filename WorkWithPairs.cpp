393408d235e8819df6f8656e5dc53ddd60ad1e78
verwindle <verwindlegmail.com>
2018-04-12 17:54:05 +0300

    Converter, functions getX(), getY() added

--- WorkWithPairs.cpp
+++ WorkWithPairs.cpp
@@ -2,16 +2,31 @@
 // Created by fantom on 05.04.18.
 //
 
+#include <utility>
+#include <iostream>
+
+float getX(std::pair<float, float> pair) {
+    float x = std::get<0>(pair);
+    return x;
+}
+
+float getY(std::pair<float, float> pair) {
+    float y = std::get<1>(pair);
+    return y;
+}
+
+Dot operator+(std::pair<float, float> FirstPair, std::pair<float, float> SecondPair){
+    float TMP1 = getX(FirstPair) + getX(SecondPair);
+    float TMP2 = getY(FirstPair) + getY(SecondPair);
+    Dot res = std::make_pair(TMP1, TMP2);
+    return res;
+};
+
+Dot operator*(std::pair<float, float> Pair, float Number){
+    float TMP1 = getX(Pair)*Number;
+    float TMP2 = getY(Pair)*Number;
+    Dot res = std::make_pair(TMP1, TMP2);
+    return res;
+};
+
 #include "WorkWithPairs.h"
-
-std::pair<float, float> operator+(std::pair<float, float> FirstPair, std::pair<float, float> SecondPair){
-    float TMP1 = std::get<0>(FirstPair) + std::get<0>(SecondPair);
-    float TMP2 = std::get<1>(FirstPair) + std::get<1>(SecondPair);
-    return std::make_pair(TMP1, TMP2);
-};
-
-std::pair<float, float> operator*(std::pair<float, float> Pair, float Number){
-    float TMP1 = std::get<0>(Pair)*Number;
-    float TMP2 = std::get<1>(Pair)*Number;
-    return std::make_pair(TMP1, TMP2);
-};
