5ecbe62d27fb893bf31acaf66852f05c1555667c
verwindle <verwindlegmail.com>
2018-04-12 17:51:41 +0300

    Converter, functions getX(), getY() added

--- ConverterPairToVector2.h
+++ ConverterPairToVector2.h
@@ -1,10 +1,6 @@
 //
 // Created by verwindle on 07.04.18. How pathetic
 //
-#include "Dot.h"
-#include "Components.h"
-#include "GameObject.h"
-#include "resources.h"
 #ifndef PROJECT_INFA_CONVERTERPAIRTOVECTOR2_H
 #define PROJECT_INFA_CONVERTERPAIRTOVECTOR2_H
 
