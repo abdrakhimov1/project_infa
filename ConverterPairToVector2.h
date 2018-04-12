f068dd9080a8439ae9867f9b5913c2d32caf687d
verwindle <verwindlegmail.com>
2018-04-12 17:49:57 +0300

    Converter, functions getX(), getY() added

---
+++ ConverterPairToVector2.h
@@ -0,0 +1,17 @@
+//
+// Created by verwindle on 07.04.18. How pathetic
+//
+#include "Dot.h"
+#include "Components.h"
+#include "GameObject.h"
+#include "resources.h"
+#ifndef PROJECT_INFA_CONVERTERPAIRTOVECTOR2_H
+#define PROJECT_INFA_CONVERTERPAIRTOVECTOR2_H
+
+sf::Vector2f getVectorFromPair(std::pair<float, float> coords);
+
+#endif //PROJECT_INFA_CONVERTERPAIRTOVECTOR2_H
+
+
+
+
